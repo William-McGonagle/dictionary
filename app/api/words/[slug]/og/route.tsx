3e7ffbae6b7ee19b8b7caea95abb05c801c92d64
William McGonagle <wpmcgonagle@gmail.com>
2024-03-14 14:35:16 -0400

    Modified Open Graph

---
+++ app/api/words/[slug]/og/route.tsx
@@ -0,0 +1,76 @@
+import { loadGoogleFont } from "@/lib/fonts";
+import { ImageResponse } from "@vercel/og";
+import React from "react";
+
+import words from "@/output.json";
+import { Noto_Serif_Display } from "next/font/google";
+const inter = Noto_Serif_Display({ subsets: ["latin"] });
+
+export const config = {
+  runtime: "edge",
+};
+
+const parts: Record<string, string> = {
+  "n.": "noun",
+  "v. i.": "verb",
+  "v. t.": "verb",
+  "a.": "adjective",
+  "adv.": "adverb",
+  "pron.": "pronoun",
+  "prep.": "preposition",
+};
+
+export async function GET(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
+  const { slug } = params;
+  // @ts-ignore
+  const wordData = words[slug] || words["lost"];
+  const fontData = await loadGoogleFont("Noto+Serif+Display");
+
+  let _parts = wordData.defs.map(
+    (i: { def: string; pos: string }) => parts[i.pos]
+  );
+  _parts = Array.from(new Set(_parts));
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 100,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          fontFamily: "NotoSerif",
+          justifyContent: "center",
+          alignItems: "center",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <span>{_parts.join(" – ")}</span>
+        <h1 className="text-2xl font-bold text-gray-900 md:text-3xl">
+          {wordData.text}
+        </h1>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "NotoSerif",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+      // Supported options: 'twemoji', 'blobmoji', 'noto', 'openmoji', 'fluent', 'fluentFlat'
+      // Default to 'twemoji'
+      // emoji: "twemoji",
+    }
+  );
+}
